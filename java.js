bea65dc8f2eb03731a60972aa9987526cee61a8a
muskan3012 <muskanvashist15@gmail.com>
2025-05-14 10:16:48 +0530

    New Update

---
+++ java.js
@@ -0,0 +1,21 @@
+let stu_name = "Muskan";
+const stu_id = "220bdes024";
+var stu_contact = "8510009889"
+console. log(stu_name);
+// initilized but update
+console. log(day);
+// value over ride
+let year = 2025;
+
+year = 2026;
+
+
+
+console.log (year);
+
+
+// can't update and re-inilitized
+const day = "Friday"
+
+// datatype
+
